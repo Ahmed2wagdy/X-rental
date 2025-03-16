9873b7d9a32c68b825fdf5fe8e6ec099eb242d74
Ahmed Wagdy <91511476+Ahmedwagdy84@users.noreply.github.com>
2025-03-16 19:56:45 +0200

    lhd-ui of loginand signup &routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter([
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'login', component: LogInComponent }, // تأكد من إضافة هذا السطر
+      { path: '', redirectTo: 'login', pathMatch: 'full' }, // توجيه افتراضي للصفحة الرئيسية
+    ])
+  ]
 };
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
